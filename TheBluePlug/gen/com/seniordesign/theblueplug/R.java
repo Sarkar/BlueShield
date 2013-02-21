415ac7be8720b5682d93f0588841d0014eec9d57
Loquen Jones <lrjones416@gmail.com>
2013-02-20 18:17:54 -0800

    Working on Splash Screen and first login screen
    Will probably remove login, go directly to all appliances

--- TheBluePlug/gen/com/seniordesign/theblueplug/R.java
+++ TheBluePlug/gen/com/seniordesign/theblueplug/R.java
@@ -25,19 +25,23 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
+        public static final int activity_splash=0x7f030001;
     }
     public static final class menu {
         public static final int activity_login=0x7f070000;
+        public static final int activity_splash=0x7f070001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int cancel=0x7f040005;
         public static final int done=0x7f040004;
+        public static final int hello_world=0x7f040008;
         public static final int login=0x7f040002;
         public static final int loginblurb=0x7f040001;
         public static final int menu_settings=0x7f040006;
         public static final int submit=0x7f040003;
         public static final int title_activity_login=0x7f040007;
+        public static final int title_activity_splash=0x7f040009;
     }
     public static final class style {
         public static final int AppTheme=0x7f060000;
