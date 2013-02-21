4d75087b21c6761964ccd09b9df7d25be2f19267
Loquen Jones <lrjones416@gmail.com>
2013-02-21 14:50:34 -0800

    working splash screen
    wrong image

--- TheBluePlug/src/com/seniordesign/theblueplug/LoginActivity.java
+++ TheBluePlug/src/com/seniordesign/theblueplug/LoginActivity.java
@@ -1,7 +1,7 @@
 package com.seniordesign.theblueplug;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
